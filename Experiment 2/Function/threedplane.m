d7ae0912e0313afc559135db92570b0cd51fb872
FengChenNS <Feng_Chen_@outlook.com>
2026-02-05 15:32:11 +0800

    Update and rename Experiment 2/threedplane.m to Experiment 2/Function/threedplane.m

--- Experiment 2/Function/threedplane.m
+++ Experiment 2/Function/threedplane.m
@@ -1,6 +1,5 @@
 function [a,b,c,d] = threedplane(x,y,z)
-%UNTITLED3 Summary of this function goes here
-% 协方差矩阵的SVD变换中，最小奇异值对应的奇异向量就是平面的方向
+
 planeData = [x,y,z];
 xyz0=mean(planeData,1);
 centeredPlane=bsxfun(@minus,planeData,xyz0);
@@ -10,7 +9,6 @@ b=V(2,3);
 c=V(3,3);
 d=-dot([a b c],xyz0);
 
-%图形绘制
 
 scatter3(x,y,z,'filled')
 hold on;
